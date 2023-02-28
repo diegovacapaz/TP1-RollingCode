9c4cc222a43f0fe384a474e580b6e2089061d981
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-02-28 12:10:07 -0300

    Se completo el punto 5

---
+++ js/punto5.js
@@ -0,0 +1,3 @@
+let firstNumber = parseInt(prompt("Ingrese el primer operando: "));
+let secondNumber = parseInt(prompt("Ingrese el segundo operando: "));
+document.write(`<p>${firstNumber + secondNumber}</p>`);
